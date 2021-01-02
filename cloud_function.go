5f190d3c572d7054019d7b5273acd8c69e27e62a
Benjamin Nørgaard <b@noergaard.dk>
2021-01-02 01:36:30 +0100

    Add decoding

--- cloud_function.go
+++ cloud_function.go
@@ -1,6 +1,7 @@
 package steve
 
 import (
+	"encoding/hex"
 	"net/http"
 	"os"
 	"sync"
@@ -13,12 +14,19 @@ var (
 
 func CloudFunction(w http.ResponseWriter, r *http.Request) {
 	cloudFunctionOnce.Do(func() {
+
 		s := New(
 			AppID(os.Getenv("STEVE_APPLICATION_ID")),
-			AppPubKey(os.Getenv("STEVE_APPLICATION_PUBKEY")),
 			BotToken(os.Getenv("STEVE_BOT_TOKEN")),
 			GuildID(os.Getenv("STEVE_GUILD_ID")),
 		)
+
+		apk, err := hex.DecodeString(os.Getenv("STEVE_APPLICATION_PUBKEY"))
+		if err != nil {
+			s.log.Error(err, "could not decode pubkey")
+		}
+
+		AppPubKey(apk)(s)
 
 		if err := s.RegisterCommands(); err != nil {
 			s.log.Error(err, "could not register commands")
